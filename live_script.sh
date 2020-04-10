33474fde81a898bb09507312b9d1625ed9544ee9
Fiifi <fiifi.krampah@gmail.com>
2020-04-09 19:53:28 -0500

    Update to GRM Live

--- live_script.sh
+++ live_script.sh
@@ -5,7 +5,7 @@ if [ "$1" != "" ]; then
     fbid=$(echo $1 | sed 's~^.*videos/~~') # delete everything before videos/. output: /id/
     echo $fbid
 	sed -i "s~videos.*/~videos/$fbid~" Live.html # replace videos/ with videos/id. output: fb video url
-    git add . && git commit -m "Update to GRM site" && git push origin master
+    git add . && git commit -m "Update to GRM Live" && git push origin master
 else
     echo "Please provide the URL of the live FB video like so: ./live_script.sh url"
 fi
